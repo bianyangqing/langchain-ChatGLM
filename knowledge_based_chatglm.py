787cdad44abe8396ee8580bdfa76bf6b28d4a56b
黄茵 <huangyin@huangyindeMacBook-Pro.local>
2023-04-14 16:20:06 +0800

    fix vector_store_result

--- knowledge_based_chatglm.py
+++ knowledge_based_chatglm.py
@@ -52,10 +52,7 @@ def init_cfg(LLM_MODEL, EMBEDDING_MODEL, LLM_HISTORY_LEN, V_SEARCH_TOP_K=6):
 
     while not vector_store_result:
         vector_store_result = init_knowledge_vector_store(os.environ.get('file_path'))
-    return vector_store_result
 
-
-vector_store_result = init_cfg(LLM_MODEL, EMBEDDING_MODEL, LLM_HISTORY_LEN)
 
 
 def init_knowledge_vector_store(filepath: str):
@@ -87,7 +84,7 @@ def init_knowledge_vector_store(filepath: str):
 
 
 def get_knowledge_based_answer(query, chat_history=[]):
-    global chatglm, embeddings
+    global chatglm, embeddings, vector_store_result
 
     prompt_template = """基于以下已知信息，简洁和专业的来回答用户的问题。
 如果无法从中得到答案，请说 "根据已知信息无法回答该问题" 或 "没有提供足够的相关信息"，不允许在答案中添加编造成分，答案请使用中文。
