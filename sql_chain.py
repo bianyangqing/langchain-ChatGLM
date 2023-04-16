95a648b40805469a167eb579b060c7537efc503c
黄茵 <huangyin@huangyindeMacBook-Pro.local>
2023-04-16 12:21:13 +0800

    add chatglm

--- sql_chain.py
+++ sql_chain.py
@@ -1,7 +1,6 @@
 from langchain import OpenAI, SQLDatabase, SQLDatabaseChain
 import os
 from chatglm_llm import ChatGLM
-from knowledge_based_chatglm import llm_model_dict, LLM_MODEL, LLM_HISTORY_LEN
 
 os.environ['OPENAI_API_KEY'] = os.environ.get('the_key_you_need')
 
@@ -10,8 +9,8 @@ llm = OpenAI(temperature=0)
 
 
 chatglm = ChatGLM()
-chatglm.load_model(model_name_or_path=llm_model_dict[LLM_MODEL])
-chatglm.history_len = LLM_HISTORY_LEN
+chatglm.load_model(model_name_or_path="THUDM/chatglm-6b-int4")
+chatglm.history_len = 3
 
 
 db_chain = SQLDatabaseChain(llm=chatglm, database=db, verbose=True)
