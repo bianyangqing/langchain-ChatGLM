90514e015fd1645abbc7e3a72ec3afc353eb735a
黄茵 <huangyin@huangyindeMacBook-Pro.local>
2023-04-16 16:10:34 +0800

    fix prompt

--- sql_chain.py
+++ sql_chain.py
@@ -1,6 +1,21 @@
-from langchain import OpenAI, SQLDatabase, SQLDatabaseChain
+from langchain import OpenAI, SQLDatabase, SQLDatabaseChain, PromptTemplate
 import os
 from chatglm_llm import ChatGLM
+
+
+
+_DEFAULT_TEMPLATE = """Given an input question, first create a syntactically correct {dialect} query to run
+
+Only use the tables listed below.
+
+{table_info}
+
+Question: {input}"""
+
+PROMPT_SIMPLE = PromptTemplate(
+    input_variables=["input", "table_info", "dialect", "top_k"],
+    template=_DEFAULT_TEMPLATE,
+)
 
 db = SQLDatabase.from_uri("sqlite:///test.db")
 llm = OpenAI(temperature=0)
@@ -11,5 +26,6 @@ chatglm.load_model(model_name_or_path="THUDM/chatglm-6b-int4")
 chatglm.history_len = 3
 
 
-db_chain = SQLDatabaseChain(llm=chatglm, database=db, verbose=True)
+db_chain = SQLDatabaseChain(llm=chatglm, database=db,
+                            prompt = PROMPT_SIMPLE, verbose=True)
 db_chain.run(os.environ['question'])
